bff5013c8027cbd2ff875f4af663c86c19d14fac
hujun <huj@hiscene.com>
2018-05-08 01:53:54 +0800

    Add : smart contract and new test page

--- js/test.js
+++ js/test.js
@@ -13,33 +13,32 @@ if(typeof(webExtensionWallet) === "undefined"){
     $("#wallet_alert").hide();
 }
 
-var dappAddress = "n1unJNMNpDDdMu2kPKyevXbVsLxPZ4ycCoe";
+var dappAddress = "n1tAXmXpU3Le2u2tJcsiZEMpDeHZ4EhPVZC";
+// var dappAddress = "n1unJNMNpDDdMu2kPKyevXbVsLxPZ4ycCoe";
+
+
 
 // 搜索功能: 查找Super-Dictionary 中有没有该词条
-$("#search").click(function(){
+$("#btn_refresh").click(function(){
 // $("#search_value").val() 搜索框内的值
 
 var to = dappAddress;
 var value = "0";
 var callFunction = "get";
-var callArgs = "[\"" + $("#search_value").val() + "\"]"; //in the form of ["args"]
+var key = "key1";
+var callArgs = "[\"" + key + "\"]"; //in the form of ["args"]
 nebPay.simulateCall(to, value, callFunction, callArgs, {    //使用nebpay的simulateCall接口去执行get查询, 模拟执行.不发送交易,不上链
 listener: cbSearch      //指定回调函数
 });
-})
+});
 
 //return of search,
 function cbSearch(resp) {
-    var result = resp.result
+    var result = resp.result;
     console.log("return of rpc call: " + JSON.stringify(result))
 
     if (result === 'null'){
-        $(".add_banner").addClass("hide");
-        $(".result_success").addClass("hide");
-
-        $("#result_faile_add").text($("#search_value").val())
-
-        $(".result_faile").removeClass("hide");
+        $("#noinfo_alert").show();
     } else{
 //if result is not null, then it should be "return value" or "error message"
 try{
@@ -47,52 +46,82 @@ try{
 }catch (err){
     //result is the error message
 }
+    var author = result.author;
+    var nickname = result.nickname;
+    var timestamp = result.timestamp;
+    var longitude = result.longitude;
+    var latitude = result.latitude;
+    var message = result.message;
+    var contact = result.contact;
 
-if (!!result.key){      //"return value"
-    $(".add_banner").addClass("hide");
-$(".result_faile").addClass("hide");
-
-$("#search_banner").text($("#search_value").val())
-$("#search_result").text(result.value)
-$("#search_result_author").text(result.author)
-
-$(".result_success").removeClass("hide");
-} else {        //"error message"
-$(".add_banner").addClass("hide");
-$(".result_faile").addClass("hide");
-
-$("#search_banner").text($("#search_value").val())
-$("#search_result").text(result)
-$("#search_result_author").text("")
-
-$(".result_success").removeClass("hide");
-}
+    console.log("message="+message);
+    $("#message").html(message);
+    $("#author").html(nickname);
+    $("#contact").html(contact);
 
 }
 
 }
 
-// 添加信息功能: 像super-dictionary 中添加词条
-$("#add").click(function() {
-    $(".result_faile").addClass("hide");
-    $(".add_banner").removeClass("hide");
 
-    $("#add_value").val("")
-})
+$("#btn_submit").click(function() {
 
-$("#push").click(function() {
 
-    var to = dappAddress;
+    getLocation();
+
+
+});
+
+function cbPush(resp) {
+    // var result = resp.result;
+    // console.log("response of push: " + result);
+    // if (result!='null') {
+
+    // };
+}
+
+function getLocation()
+        {
+            var options = {
+                enableHighAccuracy:true,
+                maximumAge:1000
+            };
+            if (navigator.geolocation) {
+                //浏览器支持geolocation
+                navigator.geolocation.getCurrentPosition(onSuccess, onError, options);//调用成功则调用onSuccess函数，失败则调用onError函数
+            } else {
+                //浏览器不支持geolocation
+                error();//调用error函数提示用户
+            }
+        }
+        function error()
+        {
+            alert("sorry , your brower is not  used   for this position!  ");
+        }
+        function onError(position)
+        {
+            console.log(position);//打印错误信息
+        }
+        function onSuccess(position)
+        {
+            console.log(position);//打印位置信息
+            var lat = position.coords.latitude; //纬度 
+            var lag = position.coords.longitude; //经度 
+             var message = $("#exampleInputContent").val();
+    console.log("message "+message);
+    var contact = $("#exampleInputContact").val();
+    var nickname = $("#exampleInputNickname").val();
+    var timestamp= new Date().getTime();
+
+        var to = dappAddress;
     var value = "0";
-    var callFunction = "save"
-    var callArgs = "[\"" + $("#search_value").val() + "\",\"" + $("#add_value").val() + "\"]"
+    var callFunction = "save";
+    var key = "key1";
+
+    var callArgs = "[\""+key+"\",\""+nickname+"\",\""+timestamp+"\",\""+lag+"\",\""+lat+"\",\""+message+"\",\""+contact+"\"]";
 
 nebPay.call(to, value, callFunction, callArgs, {    //使用nebpay的call接口去调用合约,
     listener: cbPush
 });
-});
-
-function cbPush(resp) {
-    console.log("response of push: " + resp)
-}
+        }
 
--- smartContract/infoboard.js
+++ smartContract/infoboard.js
@@ -1,32 +1,98 @@
 'use strict';
 
-var DepositeContent = function (text){
+//消息类
+var InfoItem = function (text){
     if (text) {
-        var o = JSON.parse(text);
-        this.balance = new BigNumber(o.balance);
-        this.expiryHeight = new BigNumber(o.expiryHeight);
+        var obj = JSON.parse(text);
+        //发表用户的钱包地址
+        this.author = obj.author;
+        //用户填写的昵称
+        this.nickname = obj.nickname;
+        //发表时间戳
+        this.timestamp  =obj.timestamp;
+        //地理位置坐标
+        //经纬度都保留6位有效数字
+        this.longitude = obj.longitude;
+        this.latitude = obj.latitude;
+        //发表内容
+        this.message = obj.message;
+        //联系方式
+        this.contact = obj.contact;
+
     }else{
-        this.balance = new BigNumber(0);
-        this.expiryHeight = new BigNumber(0);
+        this.author = "";
+        this.nickname = "";
+        this.timestamp = new BigNumber(0);
+        this.longitude = 0.0;
+        this.latitude = 0.0;
+        this.message = "";
+        this.contact = "";
     }
 };
 
-DepositeContent.prototype = {
-    toString : function(){
+InfoItem.prototype = {
+    toString:function(){
         return JSON.stringify(this);
     }
 };
 
-var BankVaultContract = function(){
-    LocalContractStorage.defineMapProperty(this,"bankVault",{
-        parse: function(text){
-            return new DepositeContent(text);
+//消息管理类
+var InfoManager = function(){
+    LocalContractStorage.defineMapProperty(this, "repo", {
+        parse: function (text) {
+            return new InfoItem(text);
         },
-        stringify : function(o){
+        stringify: function (o) {
             return o.toString();
         }
     });
 };
 
+InfoManager.prototype = {
+    init:function(){
+        this.size = 0;
+    },
 
+    save:function(key,nickname,timestamp,longitude,latitude,message,contact){
+        nickname = nickname.trim();
+        message = message.trim();
+        contact = contact.trim();
+        if (message === "") {
+            throw new Error("message cannot be empty");
+        };
+
+        if (message.length>256*256) {
+            throw new Error("message exceed limit length");
+        };
+
+/*
+        this.author = "";
+        this.nickname = "";
+        this.timestamp = new BigNumber(0);
+        this.longitude = 0.0;
+        this.latitude = 0.0;
+        this.message = "";
+        this.contact = "";*/
+
+        var from = Blockchain.transaction.from;
+        var info = new InfoItem();
+        info.author = from;
+        info.nickname = nickname;
+        info.timestamp = timestamp;
+        info.longitude = longitude;
+        info.latitude = latitude;
+        info.message = message;
+        info.contact = contact;
+
+        // var key = this.size;
+
+        this.repo.put(key,info);
+        this.size +=1;
+    },
+    get:function(key){
+        return this.repo.get(key);
+    }
+};
+
+module.exports = InfoManager;
 
