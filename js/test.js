b1d7f26e482ae580d971387693f711ef7c6f0f7f
hujun <huj@hiscene.com>
2018-05-09 03:01:26 +0800

    no message

--- js/test.js
+++ js/test.js
@@ -2,6 +2,7 @@ var NebPay = require("nebpay");     //https://github.com/nebulasio/nebPay
 var nebPay = new NebPay();
 
 
+
 //to check if the extension is installed
 //if the extension is installed, var "webExtensionWallet" will be injected in to web page
 if (typeof (webExtensionWallet) === "undefined") {
@@ -21,20 +22,20 @@ $("#btn_refresh").click(function () {
     refreshData();
 });
 
-function refreshData () {
+function refreshData() {
     // body...
     var to = dappAddress;
     var value = "0";
     var callFunction = "forEach";
     var start = 0;
-    var end = 10;
+    var end = allCount;
     var callArgs = "[\"" + start + "\",\"" + end + "\"]"; //in the form of ["args"]
     nebPay.simulateCall(to, value, callFunction, callArgs, {    //使用nebpay的simulateCall接口去执行get查询, 模拟执行.不发送交易,不上链
         listener: refreshPage      //指定回调函数
     });
 }
 
-function initPageData () {
+function initPageData() {
     // body...
     // body...
     var to = dappAddress;
@@ -44,16 +45,16 @@ function initPageData () {
     var end = 2;
     // var callArgs = "[\"" + start + "\",\"" + end + "\"]"; //in the form of ["args"]
     nebPay.simulateCall(to, value, callFunction, "[]", {    //使用nebpay的simulateCall接口去执行get查询, 模拟执行.不发送交易,不上链
-        listener: function(resp){
+        listener: function (resp) {
             var result = resp.result;
             allCount = parseInt(result);
-            console.log("allcount="+allCount);
+            console.log("allcount=" + allCount);
             refreshData();
         }      //指定回调函数
     });
 }
 
-
+var mapMarkers = [];
 
 function refreshPage(resp) {
     var result = resp.result;
@@ -75,38 +76,41 @@ function refreshPage(resp) {
         var arrar = result.split("|");
         var len = arrar.length;
 
-        for (var i = 0; i < arrar.length-1; i++) {
+        for (var i = 0; i < arrar.length - 1; i++) {
 
-        var result1 = arrar[i];
-        var obj = JSON.parse(result1);
+            var result1 = arrar[i];
+            var obj = JSON.parse(result1);
 
 
-        var author = obj.author;
-        var nickname = obj.nickname;
-        var timestamp = obj.timestamp;
-        var longitude = obj.longitude;
-        var latitude = obj.latitude;
-        var message = obj.message;
-        var contact = obj.contact;
+            var author = obj.author;
+            var nickname = obj.nickname;
+            var timestamp = obj.timestamp;
+            var longitude = obj.longitude;
+            var latitude = obj.latitude;
+            var message = obj.message;
+            var contact = obj.contact;
 
-        if (obj.nickname==="") {
-            nickname = author;
+            if (obj.nickname === "") {
+                nickname = author;
+            };
+
+            if (obj.contact === "") {
+                contact = "无";
+            };
+
+            var html = "<tr><td>" + message + "</td><td>" + nickname + "</td><td>" + contact + "</td></tr>";
+
+            $("#info_table").append(html);
+
+            console.log("message=" + message);
+
+            mapMarkers.push({ name: nickname, latLng: [latitude, longitude] });
+            // $("#message").html(message);
+            // $("#author").html(nickname);
+            // $("#contact").html(contact);
         };
 
-        if (obj.contact === "") {
-            contact = "无";
-        };
-
-        var html = "<tr><td>"+message+"</td><td>"+nickname+"</td><td>"+contact+"</td></tr>";
-
-        $("#info_table").append(html);
-
-        console.log("message="+message);
-        // $("#message").html(message);
-        // $("#author").html(nickname);
-        // $("#contact").html(contact);
-        };
-
+        mapObj.addMarkers(mapMarkers, []);
 
     }
 
@@ -114,11 +118,11 @@ function refreshPage(resp) {
 
 
 $("#btn_submit").click(function () {
-
-
+    var message = $("#exampleInputContent").val();
+    if (message === "") {
+        alert("请填写发布内容");
+    }else
     getLocation();
-
-
 });
 
 function cbPush(resp) {
@@ -133,6 +137,7 @@ function getLocation() {
     console.log("getLocation")
     var options = {
         enableHighAccuracy: true,
+        timeout:5000,
         maximumAge: 1000
     };
     if (navigator.geolocation) {
@@ -148,6 +153,23 @@ function error() {
 }
 function onError(position) {
     console.log(position);//打印错误信息
+    var innerHTML = "获取位置错误";
+    switch(error.code) 
+    {
+        case error.PERMISSION_DENIED:
+            innerHTML="用户拒绝对获取地理位置的请求。需要科学上网获取位置"
+            break;
+        case error.POSITION_UNAVAILABLE:
+            innerHTML="位置信息是不可用的。需要科学上网获取位置"
+            break;
+        case error.TIMEOUT:
+            innerHTML="请求用户地理位置超时。需要科学上网获取位置"
+            break;
+        case error.UNKNOWN_ERROR:
+            innerHTML="未知错误。需要科学上网获取位置"
+            break;
+    }
+    alert(innerHTML);
 }
 function onSuccess(position) {
     console.log(position);//打印位置信息
@@ -171,45 +193,20 @@ function onSuccess(position) {
     });
 }
 
-$('#world-map').vectorMap({
-        map: 'world_mill',
-        scaleColors: ['#C8EEFF', '#0071A4'],
+var mapObj = new jvm.Map({
+    container: $('#world-map'),
+    map: 'world_mill',
     normalizeFunction: 'polynomial',
+    scaleColors: ['#C8EEFF', '#0071A4'],
     hoverOpacity: 0.7,
     hoverColor: false,
     markerStyle: {
-      initial: {
-        fill: '#F8E23B',
-        stroke: '#383f47'
-      }
+        initial: {
+            fill: '#F8E23B',
+            stroke: '#383f47'
+        }
     },
     backgroundColor: '#383f47',
-    markers: [
-      {latLng: [41.90, 12.45], name: 'Vatican City'},
-      {latLng: [43.73, 7.41], name: 'Monaco'},
-      {latLng: [-0.52, 166.93], name: 'Nauru'},
-      {latLng: [-8.51, 179.21], name: 'Tuvalu'},
-      {latLng: [43.93, 12.46], name: 'San Marino'},
-      {latLng: [47.14, 9.52], name: 'Liechtenstein'},
-      {latLng: [7.11, 171.06], name: 'Marshall Islands'},
-      {latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis'},
-      {latLng: [3.2, 73.22], name: 'Maldives'},
-      {latLng: [35.88, 14.5], name: 'Malta'},
-      {latLng: [12.05, -61.75], name: 'Grenada'},
-      {latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines'},
-      {latLng: [13.16, -59.55], name: 'Barbados'},
-      {latLng: [17.11, -61.85], name: 'Antigua and Barbuda'},
-      {latLng: [-4.61, 55.45], name: 'Seychelles'},
-      {latLng: [7.35, 134.46], name: 'Palau'},
-      {latLng: [42.5, 1.51], name: 'Andorra'},
-      {latLng: [14.01, -60.98], name: 'Saint Lucia'},
-      {latLng: [6.91, 158.18], name: 'Federated States of Micronesia'},
-      {latLng: [1.3, 103.8], name: 'Singapore'},
-      {latLng: [1.46, 173.03], name: 'Kiribati'},
-      {latLng: [-21.13, -175.2], name: 'Tonga'},
-      {latLng: [15.3, -61.38], name: 'Dominica'},
-      {latLng: [-20.2, 57.5], name: 'Mauritius'},
-      {latLng: [26.02, 50.55], name: 'Bahrain'},
-      {latLng: [0.33, 6.73], name: 'São Tomé and Príncipe'}
-    ]
-      });
+    markers: []
+});
+
