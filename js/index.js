a83b46a3575482469029dfaa6400642d2fb26da0
hujun <huj@hiscene.com>
2018-05-09 10:24:49 +0800

    修改了test.html名字，改为主页

--- js/index.js
+++ js/index.js
@@ -1,47 +1,67 @@
 var NebPay = require("nebpay");     //https://github.com/nebulasio/nebPay
 var nebPay = new NebPay();
 
-$("#search_value").attr("disabled", true)
-$("#search").attr("disabled", true)
 
 
 //to check if the extension is installed
 //if the extension is installed, var "webExtensionWallet" will be injected in to web page
 if (typeof (webExtensionWallet) === "undefined") {
-    //alert ("Extension wallet is not installed, please install it first.")
-    $("#noExtension").removeClass("hide")
+
 } else {
-    $("#search_value").attr("disabled", false)
-    $("#search").attr("disabled", false)
+    $("#wallet_alert").hide();
 }
 
-var dappAddress = "n1unJNMNpDDdMu2kPKyevXbVsLxPZ4ycCoe";
+var dappAddress = "n1sgsPCNuR9sw2Nekr8qFNRtQG2ySj5fLfD";
+// var dappAddress = "n1unJNMNpDDdMu2kPKyevXbVsLxPZ4ycCoe";
+var value = "0";
+var allCount = 0;
 
-// 搜索功能: 查找Super-Dictionary 中有没有该词条
-$("#search").click(function () {
-    // $("#search_value").val() 搜索框内的值
+initPageData();
 
+$("#btn_refresh").click(function () {
+    refreshData();
+});
+
+function refreshData() {
+    // body...
     var to = dappAddress;
     var value = "0";
-    var callFunction = "get";
-    var callArgs = "[\"" + $("#search_value").val() + "\"]"; //in the form of ["args"]
+    var callFunction = "forEach";
+    var start = 0;
+    var end = allCount;
+    var callArgs = "[\"" + start + "\",\"" + end + "\"]"; //in the form of ["args"]
     nebPay.simulateCall(to, value, callFunction, callArgs, {    //使用nebpay的simulateCall接口去执行get查询, 模拟执行.不发送交易,不上链
-        listener: cbSearch      //指定回调函数
+        listener: refreshPage      //指定回调函数
     });
-})
+}
 
-//return of search,
-function cbSearch(resp) {
-    var result = resp.result
+function initPageData() {
+    // body...
+    // body...
+    var to = dappAddress;
+    var value = "0";
+    var callFunction = "len";
+    var start = 0;
+    var end = 2;
+    // var callArgs = "[\"" + start + "\",\"" + end + "\"]"; //in the form of ["args"]
+    nebPay.simulateCall(to, value, callFunction, "[]", {    //使用nebpay的simulateCall接口去执行get查询, 模拟执行.不发送交易,不上链
+        listener: function (resp) {
+            var result = resp.result;
+            allCount = parseInt(result);
+            console.log("allcount=" + allCount);
+            refreshData();
+        }      //指定回调函数
+    });
+}
+
+var mapMarkers = [];
+
+function refreshPage(resp) {
+    var result = resp.result;
     console.log("return of rpc call: " + JSON.stringify(result))
 
     if (result === 'null') {
-        $(".add_banner").addClass("hide");
-        $(".result_success").addClass("hide");
-
-        $("#result_faile_add").text($("#search_value").val())
-
-        $(".result_faile").removeClass("hide");
+        $("#noinfo_alert").show();
     } else {
         //if result is not null, then it should be "return value" or "error message"
         try {
@@ -49,52 +69,144 @@ function cbSearch(resp) {
         } catch (err) {
             //result is the error message
         }
+        console.log("result " + result);
 
-        if (!!result.key) {      //"return value"
-            $(".add_banner").addClass("hide");
-            $(".result_faile").addClass("hide");
+        $("#info_table").html("");
 
-            $("#search_banner").text($("#search_value").val())
-            $("#search_result").text(result.value)
-            $("#search_result_author").text(result.author)
+        var arrar = result.split("|");
+        var len = arrar.length;
 
-            $(".result_success").removeClass("hide");
-        } else {        //"error message"
-            $(".add_banner").addClass("hide");
-            $(".result_faile").addClass("hide");
+        for (var i = 0; i < arrar.length - 1; i++) {
 
-            $("#search_banner").text($("#search_value").val())
-            $("#search_result").text(result)
-            $("#search_result_author").text("")
+            var result1 = arrar[i];
+            var obj = JSON.parse(result1);
 
-            $(".result_success").removeClass("hide");
-        }
+
+            var author = obj.author;
+            var nickname = obj.nickname;
+            var timestamp = obj.timestamp;
+            var longitude = obj.longitude;
+            var latitude = obj.latitude;
+            var message = obj.message;
+            var contact = obj.contact;
+
+            if (obj.nickname === "") {
+                nickname = author;
+            };
+
+            if (obj.contact === "") {
+                contact = "无";
+            };
+
+            var html = "<tr><td>" + message + "</td><td>" + nickname + "</td><td>" + contact + "</td></tr>";
+
+            $("#info_table").append(html);
+
+            console.log("message=" + message);
+
+            mapMarkers.push({ name: nickname, latLng: [latitude, longitude] });
+            // $("#message").html(message);
+            // $("#author").html(nickname);
+            // $("#contact").html(contact);
+        };
+
+        mapObj.addMarkers(mapMarkers, []);
 
     }
 
 }
 
-// 添加信息功能: 像super-dictionary 中添加词条
-$("#add").click(function () {
-    $(".result_faile").addClass("hide");
-    $(".add_banner").removeClass("hide");
 
-    $("#add_value").val("")
-})
-
-$("#push").click(function () {
-
-    var to = dappAddress;
-    var value = "0";
-    var callFunction = "save"
-    var callArgs = "[\"" + $("#search_value").val() + "\",\"" + $("#add_value").val() + "\"]"
-    console.log("test call args: " + callArgs);
-    nebPay.call(to, value, callFunction, callArgs, {    //使用nebpay的call接口去调用合约,
-        listener: cbPush
-    });
+$("#btn_submit").click(function () {
+    var message = $("#exampleInputContent").val();
+    if (message === "") {
+        alert("请填写发布内容");
+    }else
+    getLocation();
 });
 
 function cbPush(resp) {
-    console.log("response of push: " + resp)
+    // var result = resp.result;
+    // console.log("response of push: " + result);
+    // if (result!='null') {
+
+    // };
 }
+
+function getLocation() {
+    console.log("getLocation")
+    var options = {
+        enableHighAccuracy: true,
+        timeout:5000,
+        maximumAge: 1000
+    };
+    if (navigator.geolocation) {
+        //浏览器支持geolocation
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, options);//调用成功则调用onSuccess函数，失败则调用onError函数
+    } else {
+        //浏览器不支持geolocation
+        error();//调用error函数提示用户
+    }
+}
+function error() {
+    alert("sorry , your brower is not  used   for this position!  ");
+}
+function onError(position) {
+    console.log(position);//打印错误信息
+    var innerHTML = "获取位置错误";
+    switch(error.code) 
+    {
+        case error.PERMISSION_DENIED:
+            innerHTML="用户拒绝对获取地理位置的请求。需要科学上网获取位置"
+            break;
+        case error.POSITION_UNAVAILABLE:
+            innerHTML="位置信息是不可用的。需要科学上网获取位置"
+            break;
+        case error.TIMEOUT:
+            innerHTML="请求用户地理位置超时。需要科学上网获取位置"
+            break;
+        case error.UNKNOWN_ERROR:
+            innerHTML="未知错误。需要科学上网获取位置"
+            break;
+    }
+    alert(innerHTML);
+}
+function onSuccess(position) {
+    console.log(position);//打印位置信息
+    var lat = position.coords.latitude; //纬度 
+    var lag = position.coords.longitude; //经度 
+    var message = $("#exampleInputContent").val();
+    console.log("message " + message);
+    var contact = $("#exampleInputContact").val();
+    var nickname = $("#exampleInputNickname").val();
+    var timestamp = new Date().getTime();
+
+    var to = dappAddress;
+    var value = "0";
+    var callFunction = "save";
+    // var key = "key1";
+
+    var callArgs = "[\"" + nickname + "\",\"" + lag + "\",\"" + lat + "\",\"" + message + "\",\"" + contact + "\"]";
+
+    nebPay.call(to, value, callFunction, callArgs, {    //使用nebpay的call接口去调用合约,
+        listener: cbPush
+    });
+}
+
+var mapObj = new jvm.Map({
+    container: $('#world-map'),
+    map: 'world_mill',
+    normalizeFunction: 'polynomial',
+    scaleColors: ['#C8EEFF', '#0071A4'],
+    hoverOpacity: 0.7,
+    hoverColor: false,
+    markerStyle: {
+        initial: {
+            fill: '#F8E23B',
+            stroke: '#383f47'
+        }
+    },
+    backgroundColor: '#383f47',
+    markers: []
+});
 
